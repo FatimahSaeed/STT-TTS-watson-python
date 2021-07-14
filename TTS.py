87090167d0af6d6e9354f69ebccc247670ffdc8f
FatimahSaeed <55546717+FatimahSaeed@users.noreply.github.com>
2021-07-15 02:01:01 +0300

    Update TTS.py

--- TTS.py
+++ TTS.py
@@ -4,13 +4,13 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 # Insert API Key in place of 
 # 'YOUR UNIQUE API KEY'
-authenticator = IAMAuthenticator('XqzbDvKtATALn-aCP4JsFSqmK90yhid-Yp45KQUWR3Il')
+authenticator = IAMAuthenticator('YOUR UNIQUE API KEY')
 tts = TextToSpeechV1(
     authenticator=authenticator
 )
 
 #Insert URL in place of 'API_URL' 
-tts.set_service_url('https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/3c98b92e-eed0-4e5c-9f7b-1e13bd38e592')
+tts.set_service_url('API_URL')
 
 with open('./speech.mp3', 'wb') as audio_file:
      res = tts.synthesize('Testing text to speech service', accept='audio/mp3', voice='en-US_AllisonV3Voice').get_result()
