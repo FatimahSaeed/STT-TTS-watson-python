87a2d0bb958c25e5aadc3da54d0df1fbdab9bff2
FatimahSaeed <55546717+FatimahSaeed@users.noreply.github.com>
2021-07-15 02:04:25 +0300

    Update STT.py

--- STT.py
+++ STT.py
@@ -9,8 +9,10 @@ with sr.Microphone() as source:
     audio = r.listen(source)
 
 # recognize speech using IBM Speech to Text
+# Insert API Key in place of 
+# 'YOUR UNIQUE API KEY'
 IBM_USERNAME = "apikey"  
-IBM_PASSWORD = "VhqyCXNpOvcg4F4k59UInw84bS7WcyBam9GedGaf7Z3M"
+IBM_PASSWORD = "YOUR UNIQUE API KEY"
 result= r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD)
 
 try:
@@ -20,6 +22,7 @@ except sr.UnknownValueError:
 except sr.RequestError as e:
     print("Could not request results from IBM Speech to Text service; {0}".format(e))
 
+    # save STT as txt file
 with open('my_result.txt',mode ='w') as file: 
    file.write("Recognized text:") 
    file.write("\n") 
